a31421f829e2c91b2fc6371fe602e164864ef20c
The Duy <trantheduy2309@gmail.com>
2023-03-02 17:52:20 +0700

    react component with hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
+  let name = "Duy dep trai";
+  let obj = { name: "tran the duy", phone: "113" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Nav />
+
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>
+          Hello world with react {name} {obj.phone}
+        </h1>
       </header>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./nav.css";
+
+export default function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a className='active' href='#home'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href='#news'>News</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
